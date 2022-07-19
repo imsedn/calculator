db1fd89c7e19cfed96731c0ce49c86a5fb5d286b
imsedn <imse30@gmail.com>
2022-07-19 19:10:01 +0300

    f

--- main.js
+++ main.js
@@ -214,3 +214,18 @@ btnPercentage.addEventListener('click', function(){
     
 });
 
+
+document.getElementsByTagName("body").addEventListener("touchstart", tapHandler);
+
+var tapedTwice = false;
+
+function tapHandler(event) {
+    if(!tapedTwice) {
+        tapedTwice = true;
+        setTimeout( function() { tapedTwice = false; }, 300 );
+        return false;
+    }
+    event.preventDefault();
+    //action on double tap goes below
+    alert('You tapped me Twice !!!');
+ }
